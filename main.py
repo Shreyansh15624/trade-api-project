ce58d0d6ad719aca327dd5d0494216d1f2695f88
Shreyansh15624 <shreyanshvajroju@gmail.com>
2026-03-17 14:39:36 +0530

    Feature: Added the Core API Endpoint

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from trade-api-project!")
+from fastapi import FastAPI, Depends
+from security import verify_auth_and_rate_limit
 
+# Initializing the Application!
+app = FastAPI(
+    title="Trade Opportuinites API",
+    desription="Analyzes market data & provides trade opportunity insights for specific sectors in India",
+    version="1.0.0",
+)
 
-if __name__ == "__main__":
-    main()
+# Creating the single GET Endpoint
+# The 'dependencies' array forces the request to pass our security checks first
+@app.get("/analyze/{sector}", dependencies=[Depends(verify_auth_and_rate_limit)])
+async def analyze_sector(sector: str):
+    # Analyzes the psecificed market sector and returns a structured markdown report
+    # Formatting the sector identifier to be lower case & stripping whitespaces tohave cleaner data
+    clean_sector = sector.lower().strip()
+
+    """
+    Next Tasks:
+    1. Add Web Search for current Market Data
+    2. Send data to Gemini for Analysis
+    3. Return Markdown Report
+    """
+
+    return {
+        "status": "success",
+        "message": f"Security passed! ready to analyze the '{clean_sector}' sector."
+    }
